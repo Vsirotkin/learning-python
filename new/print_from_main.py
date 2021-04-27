9ae08110256a328d07ecf0bf36ae52e880cd636b
Victor.Sirotkin <vsirotkin15@gmail.com>
2021-04-27 12:39:27 +0300

    executed file from local to outside

---
+++ new/print_from_main.py
@@ -0,0 +1,9 @@
+# Press the green button in the gutter to run the script.
+
+from main import print_hi
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print_hi('Hello')
+    print_hi('Good bye')
+
