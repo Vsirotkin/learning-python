c1aec15b553297df42ca94ff0ba16c7720130068
Victor.Sirotkin <vsirotkin15@gmail.com>
2021-04-28 14:47:26 +0300

    training: for and while  loops, login and pass, rock-scissor-paper game

---
+++ new/files/abcdCallStack.py
@@ -0,0 +1,26 @@
+# shows how def in def coworks
+
+def a():
+    print('a() starts')
+    b()
+    d()
+    print('a() returns')
+
+
+def b():
+    print('b() starts')
+
+    c()
+    print('b() returns')
+
+
+def c():
+    print('c() starts')
+    print('c() returns')
+
+
+def d():
+    print('d() starts')
+    print('d() returns')
+
+a()
---
+++ new/files/helloFunc2.py
@@ -0,0 +1,5 @@
+def hello(name) -> object:
+    return 'Hello, ' + name
+
+print(hello('Alice'))
+print(hello('Bob'))
