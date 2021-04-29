13bbe23ff30ebb801a53563d492127f58c7e121c
Victor.Sirotkin <vsirotkin15@gmail.com>
2021-04-29 13:39:28 +0300

    function to compare if a user-number is even or odd. exit the program by entering 1.

---
+++ new/files/Collatz.py
@@ -0,0 +1,19 @@
+# Write a function named collatz() that has one parameter named number. If number is even,
+# then collatz() should print number // 2 and return this value. If number is odd,
+# then collatz() should print and return 3 * number + 1.
+import sys
+
+def collatz(number) -> object:
+    if number % 2 == 0:
+        return print(number // 2)
+    else:
+        return print(3 * number + 1)
+
+while True:
+    user_number = int(input('enter a number: '))
+    if user_number != 1:
+        collatz(user_number)
+        continue
+    else:
+        print('good bye!')
+        sys.exit()
