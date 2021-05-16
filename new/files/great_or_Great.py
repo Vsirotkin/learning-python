58230b840ac453e8416abcc958a9a4ae17689021
Viktor Sirotkin <vsirotkin15@gmail.com>
2021-05-16 16:03:41 +0300

    finalize the file: if g - lower case otherwise - it addes TOO

---
+++ new/files/great_or_Great.py
@@ -0,0 +1,10 @@
+# practicing low and up case in string
+
+while True:
+    print('How are you?')
+    feeling = input()
+
+    if feeling.lower() == 'g':
+        print('I feel great')
+    else:
+        print('I feel Great too')
